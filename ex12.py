ccdc68a8fd4d09ef0015ff659e91f79ba5073a14
zimang <3047247998@qq.com>
2022-09-08 15:28:22 +0800

    12: python -m pydoc "func" 查看相关文档 13: from sys import argv 提供commandline 用户输入

---
+++ ex12.py
@@ -0,0 +1,7 @@
+#input with prompt
+#python -m pydoc open 打开相关文档
+age=input("How old are you?")
+print(f"So,you're {age} old")
+#strange use of input
+#只会把输入再打印一遍
+print(input())
---
+++ ex13.py
@@ -0,0 +1,9 @@
+#argv就是实参
+from sys import argv
+#解压argv,分配参数
+#提供了更多的用户输入方式
+script,first,second,third =argv
+print("The script is called:",script)
+print("Your first variable is:",first)
+print("your second variable is:",second)
+print("your third variable is:",third)
